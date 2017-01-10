ae486f7cfe51755631ddde7aeec83dfe47fad5a0
Terry Keeney <terry.keeney@razorfish.com>
2017-01-10 10:22:11 -0700

    basic subdomain routing w/ express & next

--- index.js
+++ index.js
@@ -1,49 +1,32 @@
 const express = require('express');
-const { parse } = require('url');
 const next = require('next');
 
-const nextServer = next({dev: true});
+const nextServer = next({ dev: true });
+
+const subDomainer = (req, res, nxt) => {
+    const domain = req.headers.host.split(':')[0]; //ignore ports
+    const pieces = domain.split('.');
+
+    if (pieces.length > 2) { //has AT LEAST a subdomain
+        req.subDomain = pieces[0];
+    }
+
+    nxt();
+}
 
 
 nextServer.prepare().then(() => {
 
-  const app = express();
-  
-  app.get('/', function (req, res){
-    const parsed = parse(req.url,true);
-    console.log(parsed);
+    const app = express();
+    app.use(subDomainer);
 
-    nextServer.render(req,res,'/things',req.query);
-  });
+    app.get('/', function(req, res) {
 
-  app.listen(3000, function(){
-    'Express Listening...'
-  })
+        nextServer.render(req, res, `/${req.subDomain}`, req.query);
+    });
 
-})
+    app.listen(3000, function() {
+        'Express Listening...'
+    })
 
-// var app = express();
-
-// const subDomain = (req, res, next) => {
-//   const domain = req.headers.host.split(':')[0];
-//   const sub = domain.split('.')[0];
-//   console.log(sub)
-//   req.subdomain = sub;
-//   app.use(express.static(sub));
-
-//   next()
-// }
-
-// app.use(subDomain);
-
-// app.get('/', function (req, res) {
-
-//     var test = require(`./${req.subdomain}`)
-//     test();
-//     res.sendFile('index.html', {root: `./${req.subdomain}`});
-// })
-
-
-// app.listen(3000, function () {
-//   console.log('Listening...')
-// })
+});
